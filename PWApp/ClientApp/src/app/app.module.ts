10714465cd064901a71202d87883d1d52ca297ce
Magomed Dadagov <grozniy.info@gmail.com>
2019-05-03 01:12:49 +0300

    transfer

--- PWApp/ClientApp/src/app/app.module.ts
+++ PWApp/ClientApp/src/app/app.module.ts
@@ -1,37 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 
-import { AppComponent } from './app.component';
-import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
-import { HomeComponent } from './components/home/home.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './components/home/home.component';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AccountService } from "./services/account.service";
-import { MaterialModule } from "./material.module";
-import { BREAKPOINTS, DEFAULT_BREAKPOINTS, FlexLayoutModule } from "@angular/flex-layout";
-import { AuthComponent } from './components/auth/auth.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AccountService} from "./services/account.service";
+import {MaterialModule} from "./material.module";
+import {BREAKPOINTS, DEFAULT_BREAKPOINTS, FlexLayoutModule} from "@angular/flex-layout";
+import {AuthComponent} from './components/auth/auth.component';
 import {CookieService} from "ngx-cookie-service";
-import { AppNavBarComponent } from './components/app-nav-bar/app-nav-bar.component';
-import { TransactionsListComponent } from './components/transactions-list/transactions-list.component';
-import { NewTransactionComponent } from './components/new-transaction/new-transaction.component';
+import {AppNavBarComponent} from './components/app-nav-bar/app-nav-bar.component';
+import {TransactionsListComponent} from './components/transactions-list/transactions-list.component';
+import {NewTransactionComponent} from './components/new-transaction/new-transaction.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
     HomeComponent,
     AuthComponent,
     AppNavBarComponent,
     TransactionsListComponent,
     NewTransactionComponent
   ],
-  entryComponents:[NewTransactionComponent],
+  entryComponents: [NewTransactionComponent],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    BrowserModule.withServerTransition({appId: 'ng-cli-universal'}),
     HttpClientModule,
     FormsModule,
     AppRoutingModule,
